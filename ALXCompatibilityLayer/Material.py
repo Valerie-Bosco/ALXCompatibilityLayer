60ac2cc8129773065650bcdee649be5865d42092
Valery-AA <valeryatart@gmail.com>
2026-01-11 17:51:52 +0100

    feat: output node utilities

--- ALXCompatibilityLayer/Material.py
+++ ALXCompatibilityLayer/Material.py
@@ -20,3 +20,13 @@ def NG_new_input(node_group, name: str, socket_type: str):
             return node_group.interface.new_socket(
                 name, in_out="INPUT", socket_type=socket_type
             )
+
+
+def NG_new_output(node_group, name: str, socket_type: str):
+    match get_version():
+        case [3, 6]:
+            return node_group.inputs.new(socket_type, name)
+        case _:
+            return node_group.interface.new_socket(
+                name, in_out="OUTPUT", socket_type=socket_type
+            )
